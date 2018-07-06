d0afc256c6a8479b57b79227f291cf4c9898cb16
ARCHIT PANDEY <pandeyrishabh40@gmail.com>
2018-07-06 17:40:26 +0530

    Client Panel Web application
    Client Panel web Application used to store clients details based on Angular 4 and using firebase as a backend.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,89 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { FlashMessagesModule, FlashMessagesService } from 'angular2-flash-messages'; 
+
+// AngularFire Imports
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabase } from 'angularfire2/database-deprecated';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 
 
+// Components Imports
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ClientsComponent } from './components/clients/clients.component';
+import { ClientDetailsComponent } from './components/client-details/client-details.component';
+import { AddClientComponent } from './components/add-client/add-client.component';
+import { EditClientComponent } from './components/edit-client/edit-client.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { SettingsComponent } from './components/settings/settings.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
+// Service Imports
+import { ClientService } from './services/client.service';
+import { AuthService } from './services/auth.service';
+import { AuthGuard } from './guard/auth.guard';
+import { RegisterGuard } from './guard/register.guard';
+import { SettingsService } from './services/settings.service';
+
+const appRoutes: Routes = [
+  {path:'', component:DashboardComponent, canActivate:[AuthGuard]},
+  {path:'register', component:RegisterComponent, canActivate:[RegisterGuard]},
+  {path:'login', component:LoginComponent},
+  {path:'add-client', component:AddClientComponent, canActivate:[AuthGuard]},
+  {path:'client/:id', component:ClientDetailsComponent, canActivate:[AuthGuard]},
+  {path:'edit-client/:id', component:EditClientComponent, canActivate:[AuthGuard]},
+  {path:'settings', component:SettingsComponent, canActivate:[AuthGuard]},
+  {path:'**', component:PageNotFoundComponent}
+];
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyB3ShUE6l4k5xxkepdxnj-hn77JdGiZra0",
+    authDomain: "clientpanel-961da.firebaseapp.com",
+    databaseURL: "https://clientpanel-961da.firebaseio.com",
+    storageBucket: "clientpanel-961da.appspot.com",
+    messagingSenderId: "1036238828855"
+}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    ClientsComponent,
+    ClientDetailsComponent,
+    AddClientComponent,
+    EditClientComponent,
+    NavbarComponent,
+    SidebarComponent,
+    LoginComponent,
+    RegisterComponent,
+    SettingsComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    FlashMessagesModule
   ],
-  providers: [],
+  providers: [
+    AngularFireAuth,
+    AngularFireDatabase,
+    ClientService,
+    FlashMessagesService,
+    AuthService,
+    AuthGuard,
+    RegisterGuard,
+    SettingsService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
